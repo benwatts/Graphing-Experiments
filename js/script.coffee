0847e97359151b1acfe496ad4d7e936664c618dc
Ben Watts <the.ben.watts@gmail.com>
2012-03-01 12:18:44 -0500

    Gasp! A GREY BOX APPEARS

--- js/script.coffee
+++ js/script.coffee
@@ -1,29 +1,34 @@
 $(document).ready ->
   #chart = DashboardChart
 
-  window.myFirstChart = Graph('graph-revenue', 778, 200)
-  console.log('here?')
-
+  window.myFirstChart = Graph('graph-revenue')
 
 ## ALALALALA
 class Graph
 
-  @padding =
-    top:    20
-    right:  20
-    bottom: 20
-    left:   20
+  constructor: (domid, options={}) -> 
+    @domid = domid
 
-  constructor: (domid, width, height, opts={}) -> 
-    @domid        = domid
-    @width        = width
-    @height       = height
+    @opt = $.extend
+      height  : 200,
+      width   : 800,
+      padding :
+        top: 20, 
+        right: 20, 
+        bottom: 20, 
+        left: 20
+      options
 
-    console.log(@domid)
+    if document.getElementById(@domid)?
+      @paper = Raphael @domid, @opt.width, @opt.height
 
-    if document.getElementById(@domid).length
-      @paper = Raphael(@domid)
-      console.log('here')
+      # draw a rect that's the same size as the canvas for debugging 
+
+      bg = @paper.rect(0,0, @opt.width, @opt.height)
+      bg.attr('fill', '#ddd')
+      bg.attr('stroke-width', 0)
+
+      @paper.linechart(0, 0, @opt.width, @opt.height, [0..7], [10,15,15,1,15,20,30], { shade: true, symbol: 'circle', gutter: 0 })
 
 
 class GraphTimeline
--- js/script.js
+++ js/script.js
@@ -1,1 +1,57 @@
-((function(){var a,b,c;$(document).ready(function(){window.myFirstChart=a("graph-revenue",778,200);return console.log("here?")});a=function(){function a(a,b,c,d){d==null&&(d={});this.domid=a;this.width=b;this.height=c;console.log(this.domid);if(document.getElementById(this.domid).length){this.paper=Raphael(this.domid);console.log("here")}}a.padding={top:20,right:20,bottom:20,left:20};return a}();c=function(){function a(){}return a}();b=function(){function a(){}return a}()})).call(this);
+(function() {
+  var Graph, GraphData, GraphTimeline;
+
+  $(document).ready(function() {
+    return window.myFirstChart = Graph('graph-revenue');
+  });
+
+  Graph = (function() {
+
+    function Graph(domid, options) {
+      var bg;
+      if (options == null) options = {};
+      this.domid = domid;
+      this.opt = $.extend({
+        height: 200,
+        width: 800,
+        padding: {
+          top: 20,
+          right: 20,
+          bottom: 20,
+          left: 20
+        }
+      }, options);
+      if (document.getElementById(this.domid) != null) {
+        this.paper = Raphael(this.domid, this.opt.width, this.opt.height);
+        bg = this.paper.rect(0, 0, this.opt.width, this.opt.height);
+        bg.attr('fill', '#ddd');
+        bg.attr('stroke-width', 0);
+        this.paper.linechart(0, 0, this.opt.width, this.opt.height, [0, 1, 2, 3, 4, 5, 6, 7], [10, 15, 15, 1, 15, 20, 30], {
+          shade: true,
+          symbol: 'circle',
+          gutter: 0
+        });
+      }
+    }
+
+    return Graph;
+
+  })();
+
+  GraphTimeline = (function() {
+
+    function GraphTimeline() {}
+
+    return GraphTimeline;
+
+  })();
+
+  GraphData = (function() {
+
+    function GraphData() {}
+
+    return GraphData;
+
+  })();
+
+}).call(this);
