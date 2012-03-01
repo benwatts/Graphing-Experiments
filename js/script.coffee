17e9d2a77c79a4f48caf4cf36f8ca0a309153ff5
Ben Watts <ben.watts@jadedpixel.com>
2012-03-01 17:32:13 -0500

    Alrighty... data is somewhat normalized. rough column widths rendered

--- js/script.coffee
+++ js/script.coffee
@@ -1,36 +1,87 @@
 $(document).ready ->
-  #chart = DashboardChart
+  myFirstChart = new Graph('graph-revenue',testJson.periodical_facts.data, 'visits_count', {debug: true} )
 
-  window.myFirstChart = Graph('graph-revenue')
-
-## ALALALALA
 class Graph
 
-  constructor: (domid, options={}) -> 
-    @domid = domid
+  constructor: (domId, rawData, dataKey, options={}) -> 
+    @domId    = domId
+    @rawData  = rawData
+    @dataKey  = dataKey
+    @opt      = $.extend
+                  debug  : false,
+                  height : 200,
+                  width  : 800,
+                  gutter :
+                    top: 10, 
+                    right: 0, 
+                    bottom: 20, 
+                    left: 40
+                  options
 
-    @opt = $.extend
-      height  : 200,
-      width   : 800,
-      padding :
-        top: 20, 
-        right: 20, 
-        bottom: 20, 
-        left: 20
-      options
+    @chartX      = @opt.gutter.left
+    @chartY      = @opt.gutter.top
+    @chartWidth  = @opt.width  - @opt.gutter.left - @opt.gutter.right
+    @chartHeight = @opt.height - @opt.gutter.top  - @opt.gutter.bottom
+    @maxVal = @minVal = 0    
 
-    if document.getElementById(@domid)?
-      @paper = Raphael @domid, @opt.width, @opt.height
+    if document.getElementById(@domId)?
+      @paper = Raphael @domId, @opt.width, @opt.height
 
-      # draw a rect that's the same size as the canvas for debugging 
-
+      # draw a rect that's the same size as the canvas (debugging)
       bg = @paper.rect(0,0, @opt.width, @opt.height)
-      bg.attr('fill', '#ddd')
-      bg.attr('stroke-width', 0)
+      bg.attr('stroke-width', 1)
 
-      @paper.linechart(0, 0, @opt.width, @opt.height, [0..7], [10,15,15,1,15,20,30], { shade: true, symbol: 'circle', gutter: 0 })
+      @highlightColumns()                
+
+      @data = @normalizedData()
+
+      @paper.linechart(@chartX, @chartY, @chartWidth, @chartHeight, [0..@values.length-1], @values, { shade: true, symbol: 'circle', gutter: 0.1 })
 
 
-class GraphTimeline
+  normalizedData: ->
+    numDataPoints = @rawData.length - 1
+    dataPoints = []
+    @values = []
 
-class GraphData
+    for i in [0..numDataPoints]
+      item = @rawData[i]
+      @values.push(item[@dataKey])
+      dataPoints.push new DataPoint(item[@dataKey], 'prettyValue', item['date'], item['show'] )
+
+    @maxVal = Math.max.apply( Math, @values );
+    @minVal = Math.min.apply( Math, @values );
+
+    return dataPoints
+
+
+  highlightColumns: ->
+    numDataPoints = @rawData.length
+    colWidth = (@chartWidth/numDataPoints) # no subpixel column widths 
+    colWidthRemainder = @chartWidth - numDataPoints * colWidth
+
+    #highlights alternating columns 
+    for point in [0..numDataPoints]
+      col = @paper.rect(point*colWidth+@opt.gutter.left, @opt.gutter.top, colWidth, @chartHeight)
+      fillColour = if point % 2 == 0 then '#ff0' else '#09f'
+      col.attr('fill', fillColour)
+      col.attr('stroke-width', 0)
+
+
+
+
+### 
+  DATAPOINT! 
+  ALALALALALALALALALALAL
+
+       value -- raw value 
+ prettyValue -- value to display in bubble
+      xLabel -- label for x axis
+  showXLabel -- show label on x axis?
+
+###
+class DataPoint 
+  constructor: (value, prettyValue, xLabel, showXLabel) ->
+    @value       = value
+    @prettyValue = prettyValue
+    @xLabel      = xLabel
+    @showXLabel  = showXLabel
--- js/script.js
+++ js/script.js
@@ -1,56 +1,109 @@
 (function() {
-  var Graph, GraphData, GraphTimeline;
+  var DataPoint, Graph;
 
   $(document).ready(function() {
-    return window.myFirstChart = Graph('graph-revenue');
+    var myFirstChart;
+    return myFirstChart = new Graph('graph-revenue', testJson.periodical_facts.data, 'visits_count', {
+      debug: true
+    });
   });
 
   Graph = (function() {
 
-    function Graph(domid, options) {
-      var bg;
+    function Graph(domId, rawData, dataKey, options) {
+      var bg, _i, _ref, _results;
       if (options == null) options = {};
-      this.domid = domid;
+      this.domId = domId;
+      this.rawData = rawData;
+      this.dataKey = dataKey;
       this.opt = $.extend({
+        debug: false,
         height: 200,
         width: 800,
-        padding: {
-          top: 20,
-          right: 20,
+        gutter: {
+          top: 10,
+          right: 0,
           bottom: 20,
-          left: 20
+          left: 40
         }
       }, options);
-      if (document.getElementById(this.domid) != null) {
-        this.paper = Raphael(this.domid, this.opt.width, this.opt.height);
+      this.chartX = this.opt.gutter.left;
+      this.chartY = this.opt.gutter.top;
+      this.chartWidth = this.opt.width - this.opt.gutter.left - this.opt.gutter.right;
+      this.chartHeight = this.opt.height - this.opt.gutter.top - this.opt.gutter.bottom;
+      this.maxVal = this.minVal = 0;
+      if (document.getElementById(this.domId) != null) {
+        this.paper = Raphael(this.domId, this.opt.width, this.opt.height);
         bg = this.paper.rect(0, 0, this.opt.width, this.opt.height);
-        bg.attr('fill', '#ddd');
-        bg.attr('stroke-width', 0);
-        this.paper.linechart(0, 0, this.opt.width, this.opt.height, [0, 1, 2, 3, 4, 5, 6, 7], [10, 15, 15, 1, 15, 20, 30], {
+        bg.attr('stroke-width', 1);
+        this.highlightColumns();
+        this.data = this.normalizedData();
+        this.paper.linechart(this.chartX, this.chartY, this.chartWidth, this.chartHeight, (function() {
+          _results = [];
+          for (var _i = 0, _ref = this.values.length - 1; 0 <= _ref ? _i <= _ref : _i >= _ref; 0 <= _ref ? _i++ : _i--){ _results.push(_i); }
+          return _results;
+        }).apply(this), this.values, {
           shade: true,
           symbol: 'circle',
-          gutter: 0
+          gutter: 0.1
         });
       }
     }
+
+    Graph.prototype.normalizedData = function() {
+      var dataPoints, i, item, numDataPoints;
+      numDataPoints = this.rawData.length - 1;
+      dataPoints = [];
+      this.values = [];
+      for (i = 0; 0 <= numDataPoints ? i <= numDataPoints : i >= numDataPoints; 0 <= numDataPoints ? i++ : i--) {
+        item = this.rawData[i];
+        this.values.push(item[this.dataKey]);
+        dataPoints.push(new DataPoint(item[this.dataKey], 'prettyValue', item['date'], item['show']));
+      }
+      this.maxVal = Math.max.apply(Math, this.values);
+      this.minVal = Math.min.apply(Math, this.values);
+      return dataPoints;
+    };
+
+    Graph.prototype.highlightColumns = function() {
+      var col, colWidth, colWidthRemainder, fillColour, numDataPoints, point, _results;
+      numDataPoints = this.rawData.length;
+      colWidth = this.chartWidth / numDataPoints;
+      colWidthRemainder = this.chartWidth - numDataPoints * colWidth;
+      _results = [];
+      for (point = 0; 0 <= numDataPoints ? point <= numDataPoints : point >= numDataPoints; 0 <= numDataPoints ? point++ : point--) {
+        col = this.paper.rect(point * colWidth + this.opt.gutter.left, this.opt.gutter.top, colWidth, this.chartHeight);
+        fillColour = point % 2 === 0 ? '#ff0' : '#09f';
+        col.attr('fill', fillColour);
+        _results.push(col.attr('stroke-width', 0));
+      }
+      return _results;
+    };
 
     return Graph;
 
   })();
 
-  GraphTimeline = (function() {
+  /* 
+    DATAPOINT! 
+    ALALALALALALALALALALAL
+  
+         value -- raw value 
+   prettyValue -- value to display in bubble
+        xLabel -- label for x axis
+    showXLabel -- show label on x axis?
+  */
 
-    function GraphTimeline() {}
+  DataPoint = (function() {
 
-    return GraphTimeline;
+    function DataPoint(value, prettyValue, xLabel, showXLabel) {
+      this.value = value;
+      this.prettyValue = prettyValue;
+      this.xLabel = xLabel;
+      this.showXLabel = showXLabel;
+    }
 
-  })();
-
-  GraphData = (function() {
-
-    function GraphData() {}
-
-    return GraphData;
+    return DataPoint;
 
   })();
 
